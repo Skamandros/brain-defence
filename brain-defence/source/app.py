a5497289bc51bbf813dd41d070a892f74e51fb3b
romankempt <roman.kempt@web.de>
2024-01-26 23:06:23 +0100

    changed folder structure + app

---
+++ brain-defence/source/app.py
@@ -0,0 +1,25 @@
+import pyglet
+
+pyglet.resource.path = ["../resources"]
+pyglet.resource.reindex()
+
+window = pyglet.window.Window()
+label = pyglet.text.Label(
+    "Hello, world",
+    font_name="Times New Roman",
+    font_size=36,
+    x=window.width // 2,
+    y=window.height // 2,
+    anchor_x="center",
+    anchor_y="center",
+)
+
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+
+if __name__ == "__main__":
+    pyglet.app.run()
