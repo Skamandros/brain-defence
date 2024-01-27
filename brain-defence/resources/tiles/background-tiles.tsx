8693ea49cb3a82cc0449d4753dfa2dd9aae79c66
Papa-Oditz <accounts@oditz-gaming.de>
2024-01-27 17:11:59 +0100

    added first assets

---
+++ brain-defence/resources/tiles/background-tiles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="background-tiles" tilewidth="128" tileheight="128" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="background-tile-2.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="background-tile-3.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="tile-background-1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="path-tile.png"/>
+ </tile>
+</tileset>
