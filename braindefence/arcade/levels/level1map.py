d5dc413a15932997782dc5f4af81ef5d5853d8de
Merando <merando@gmx.de>
2024-01-28 00:29:53 +0100

    added on_update and first minion spawn

--- braindefence/arcade/levels/level1map.py
+++ braindefence/arcade/levels/level1map.py
@@ -1,3 +1,8 @@
+import arcade
+
+from braindefence import RESOURCE_DIR
+from braindefence.arcade.constants import World
+from braindefence.arcade.entities import Impression
 from braindefence.arcade.levels import BaseMap
 
 
@@ -5,3 +10,24 @@ class Level1Map(BaseMap):
 
     def __init__(self):
         super(Level1Map, self).__init__(1)
+        arcade.schedule(self.spawn_minions, 3)
+        arcade.schedule(self.spawn_minions, 5)
+
+    def spawn_minions(self, dt):
+        # in the first level we spawn basic minions with very general durability and attributes
+        impression = Impression(
+            RESOURCE_DIR.joinpath("minion-template.png").resolve(), 0.1
+        )
+        self.impressions.append(impression)
+
+        # for i, impression in enumerate(self.impressions):
+        #     impression.update(dt)
+        #     if impression.killed():
+        #         self.enemy_killed()
+        #         self.impressions.remove(impression)
+        #     elif impression.passed():
+        #         self.enemy_leaked()
+        #         self.impressions.remove(impression)
+
+    def update(self, delta_time):
+        super()
