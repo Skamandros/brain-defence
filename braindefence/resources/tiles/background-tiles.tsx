3b21fbefcc9c62ce28a9998655ead1103561793c
Papa-Oditz <accounts@oditz-gaming.de>
2024-01-27 21:34:09 +0100

    rescale asset tiles

--- braindefence/resources/tiles/background-tiles.tsx
+++ braindefence/resources/tiles/background-tiles.tsx
@@ -1,16 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="background-tiles" tilewidth="128" tileheight="128" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="background-tiles" tilewidth="128" tileheight="128" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="128" height="128" source="background-tile-2.png"/>
+  <image width="32" height="32" source="background-tile-2.png"/>
  </tile>
  <tile id="1">
-  <image width="128" height="128" source="background-tile-3.png"/>
- </tile>
- <tile id="2">
-  <image width="128" height="128" source="tile-background-1.png"/>
+  <image width="32" height="32" source="background-tile-3.png"/>
  </tile>
  <tile id="3">
-  <image width="128" height="128" source="path-tile.png"/>
+  <image width="32" height="32" source="path-tile.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="background-tile-1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="tower-tile-base.png"/>
  </tile>
 </tileset>
