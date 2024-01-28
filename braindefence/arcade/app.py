83806d4e6b9356e142784f082c68e838bde410d9
Michael Schmeißer <misc@skamandros.de>
2024-01-28 14:20:24 +0100

    Implemented a SoundManager to conveniently handle music and voice

--- braindefence/arcade/app.py
+++ braindefence/arcade/app.py
@@ -4,6 +4,7 @@ import arcade
 from pathlib import Path
 
 from braindefence.arcade.levels import LevelOneMap
+from braindefence.arcade.sound import SoundManager, BackgroundMusic
 from constants import *
 
 from braindefence import RESOURCE_DIR
@@ -22,11 +23,13 @@ class BrainDefence(arcade.Window):
         self.gui_camera = None
         self.imagination_score = 0
         self.increment_score = 0
-        self.bg_music = []
-        self.bg_music_playing = 0
-        self.track_from = None
-        self.track_to = None
-        self.fade_progress = 1
+        # Set up the protagonist
+        image_source = RESOURCE_DIR.joinpath("images/protagonist_nobg.png").resolve()
+        self.protagonist = arcade.Sprite(image_source.resolve(), .4)
+        self.protagonist.center_x = World.Width * 0.9
+        self.protagonist.center_y = self.protagonist.height / 2
+        self.protagonist.visible = False
+        self.sound_manager = SoundManager()
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
@@ -47,39 +50,8 @@ class BrainDefence(arcade.Window):
             increment_score=self.increment_score,
         )
         self.current_map.scene.add_sprite("Imagination_score", self.score_icon)
-        # Set up the protagonist
-        image_source = RESOURCE_DIR.joinpath("images/protagonist_nobg.png").resolve()
-        protagonist = arcade.Sprite(image_source.resolve(), .4)
-        protagonist.center_x = World.Width * 0.9
-        protagonist.center_y = protagonist.height / 2
-        self.current_map.scene.add_sprite("Protagonist", protagonist)
-
-        track1 = arcade.load_sound(RESOURCE_DIR.joinpath("sound/brain_1-01.wav"), True)
-        self.bg_music.append(arcade.play_sound(track1, looping=True, volume=1))
-        track2 = arcade.load_sound(RESOURCE_DIR.joinpath("sound/brain_2-01.wav"), True)
-        self.bg_music.append(arcade.play_sound(track2, looping=True, volume=0))
-        track3 = arcade.load_sound(RESOURCE_DIR.joinpath("sound/brain_3-01.wav"), True)
-        self.bg_music.append(arcade.play_sound(track3, looping=True, volume=0))
-        track4 = arcade.load_sound(RESOURCE_DIR.joinpath("sound/brain_4-01.wav"), True)
-        self.bg_music.append(arcade.play_sound(track4, looping=True, volume=0))
-        track5 = arcade.load_sound(RESOURCE_DIR.joinpath("sound/brain_5-01.wav"), True)
-        self.bg_music.append(arcade.play_sound(track5, looping=True, volume=0))
-        for i, track in enumerate(self.bg_music):
-            if i == self.bg_music_playing:
-                track.volume = 1
-            else:
-                track.volume = 0
-        arcade.schedule(self._switch_music, 10)
-
-        level01intro = arcade.load_sound(RESOURCE_DIR.joinpath("sound/Level01_Intro.mp3"), True)
-        arcade.play_sound(level01intro)
-
-    def _switch_music(self, delta_time: float):
-        self.track_from = self.bg_music_playing
-        self.bg_music_playing = (self.bg_music_playing + 1) % len(self.bg_music)
-        self.track_to = self.bg_music_playing
-        self.fade_progress = 0
-        logging.info("Fade from {} to {}".format(self.track_from, self.track_to))
+        self.current_map.scene.add_sprite("Protagonist", self.protagonist)
+        self.sound_manager.play_intro_sound(1)
 
     def on_draw(self):
         """Render the screen."""
@@ -116,11 +88,15 @@ class BrainDefence(arcade.Window):
         self.imagination_score += self.increment_score
         self.score_icon.update()
 
-        if self.fade_progress < 1:
-            self.fade_progress += delta_time / 3
-            self.bg_music[self.track_from].volume = 1 - self.fade_progress
-            self.bg_music[self.track_to].volume = self.fade_progress
-            logging.info("Fade progress {}".format(self.fade_progress))
+        if self.increment_score == 1 and self.imagination_score == 10:
+            self.sound_manager.play_event_sound(1, 1)
+            self.sound_manager.switch_bg_music(BackgroundMusic.Negative)
+        elif self.increment_score == 1 and self.imagination_score == 20:
+            self.sound_manager.play_event_sound(1, 2)
+            self.sound_manager.switch_bg_music(BackgroundMusic.Psycho)
+
+        self.sound_manager.on_update(delta_time)
+        self.protagonist.visible = self.sound_manager.is_sound_playing()
 
 
 def main():
